1604a536d97926f62fba7154604971f5434698b2
765132065@qq.com <765132065@qq.com>
2017-08-24 08:56:59 +0800

    iconfont 配置

---
+++ pages/comp/icon.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <icon-svg icon-class="jiedianyoujian">xxx</icon-svg>
+
+    </div>
+</template>
+
+
+<script>
+    export default {
+    }
+</script>
+<style>
+
+</style>
---
+++ plugins/icon-svg.js
@@ -0,0 +1,4 @@
+import IconSvg from '~/components/Icon-svg'// svg 组件
+import Vue from 'vue'
+
+Vue.component('icon-svg', IconSvg)
